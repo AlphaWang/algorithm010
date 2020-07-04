f4e890abc7575f50f4dcb3743f1553da580b631d
AlphaWang <1919wang@gmail.com>
2020-07-04 22:10:35 +0800

    alg week-4

--- Week04/NOTE.md
+++ Week04/NOTE.md
@@ -19,16 +19,16 @@ https://leetcode.com/problems/assign-cookies/
 
 - [x] 874: 模拟行走机器人 `*****` `E`
 https://leetcode.com/problems/walking-robot-simulation/
-  > 逐个解析 commands，
-  >   如果是-1/-2，则更新方向
-  >   其他情况，for 0-command, 更新res = max(res, x*x + y*y)
-  >            注意如果碰到obstacle，则不动
+  > 逐个解析 commands  ，
+  >   如果是-1/-2，则更新方向  
+  >   其他情况，for 0-command, 更新res = max(res, x*x + y*y)  
+  >            注意如果碰到obstacle，则不动  
 
 Medium
 - [x] 127: 单词接龙 `*****` `M` (!!!)
 https://leetcode.com/problems/word-ladder/
-  > 1: DFS //TODO
-  > 2: BFS. 先构造patternMap，再从beginWord开始按层次遍历
+  > 1: DFS //TODO  
+  > 2: BFS. 先构造patternMap，再从beginWord开始按层次遍历  
   > 3: BFS 优化。分别从 beginWord/endWord 往中间夹逼
 
 - [x] 200: 岛屿数量 `*****` `M`
@@ -46,25 +46,52 @@ https://leetcode.com/problems/minesweeper/
 
 - [x] 55: 跳跃游戏 `*****` `M`
 https://leetcode.com/problems/jump-game/
-  > 1: 从前往后遍历，将能走到的索引置为 true
-  > 2: 贪心：从前往后遍历，维护最大可达位置
+  > 1: 从前往后遍历，将能走到的索引置为 true  
+  > 2: 贪心：从前往后遍历，维护最大可达位置   
   > 3: 贪心：从后往前遍历，if (nums[i] + i >= endReachable) endReachable = i;
 
 - [x] 33: 旋转数组的某个值(无重复值) `*****` `M`  
 https://leetcode.com/problems/search-in-rotated-sorted-array/discuss/14425/Concise-O(log-N)-Binary-search-solution
-  > 1: 暴力解法，遍历一遍 O(N)
-  > 2: 暴力解法优化：找到最小值，还原成有序数组 O(N) or O(logN)
-  > 3: 二分法：左右侧总有一个是有序的
+  > 1: 暴力解法，遍历一遍 O(N)  
+  > 2: 暴力解法优化：找到最小值，还原成有序数组 O(N) or O(logN)  
+  > 3: 二分法：左右侧总有一个是有序的  
 
 - [x] 74: 搜索二维矩阵 `*****` `M`
 https://leetcode.com/problems/search-a-2d-matrix/
-  > 1: 两步走：先二分找到 子数组；再查子数组
-  > 2: 矩阵其实是 M * N 的有序数组
+  > 1: 两步走：先二分找到 子数组；再查子数组  
+  > 2: 矩阵其实是 M * N 的有序数组  
 
 - [x] 153: 旋转数组最小值  `*****` `M`  
 https://leetcode.com/problems/find-minimum-in-rotated-sorted-array
-  > 1: 遍历，如果nums[i] < num[i-1]，则找到。O(N)
-  > 2: 二分查找：if nums[mid] < nums[right], 往左侧找 
-  >             if nums[mid] > nums[right], 往右侧找
+  > 1: 遍历，如果nums[i] < num[i-1]，则找到。O(N)  
+  > 2: 二分查找：if nums[mid] < nums[right], 往左侧找   
+  >             if nums[mid] > nums[right], 往右侧找  
+
+- [x] 126: 单词接龙2 `*` `H`
+https://leetcode.com/problems/word-ladder-ii/
+  > 126: 找出"所有"从 beginWord 到 endWord 的最短转换序列  
+  > 127: 找到从 beginWord 到 endWord 的最短转换序列的长度   
+  > 1: BFS, queue存储当前处理的路径（单词列表） --》 TODO 执行超时   
+
+
+- [x] 45: 跳跃游戏2 `*****` `H`
+https://leetcode.com/problems/jump-game-ii/
+  > 1: 动态规划 --TODO 击败11%，太慢  
+  >      状态: dp[n] = 索引 n 到最后位置的 minSteps  
+  >      状态转移方程：dp[n] = min{dp[n + 1...nums[n]]}   
+  > 2: BFS, --TODO 超时  
+  > 3: 贪心，从后往前查找，每次找出跨度最大的位置  
+  > 4: 贪心  ，
+  >    Let's say the range of the current jump is [curBegin, curEnd],   
+  >    `reachable` is the farthest point that all points in [curBegin, curEnd] can reach.   
+  >    
+  >    Once the current point reaches curEnd, then trigger another jump,   
+  >    and set the new curEnd with `reachable`
+
+
+- [x] 433: 最小基因变化 `*****` `M`
+https://leetcode.com/problems/minimum-genetic-mutation/
+  > 1: BFS, 类似127 单词接龙
+
 
 ## 预习
---
+++ Week04/src/main/java/com/alphawang/algorithm/week04/T0045_JumpGame2.java
@@ -0,0 +1,193 @@
+package com.alphawang.algorithm.week04;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://leetcode.com/problems/jump-game-ii/
+ * Hard
+ * 
+ * 给定一个非负整数数组，你最初位于数组的第一个位置。
+ * 数组中的每个元素代表你在该位置可以跳跃的最大长度。
+ * 你的目标是使用最少的跳跃次数到达数组的最后一个位置。
+ */
+public class T0045_JumpGame2 {
+
+    /**
+     * 1. DP
+     *    状态: dp[n] = 索引 n 到最后位置的 minSteps
+     *    状态转移方程：dp[n] = min{dp[n + 1], ..., dp[n + nums[n]]}
+     *    
+     *    452ms - 11%
+     *    378ms - 17%  优化循环跳出条件
+     */
+    public int jump(int[] nums) {
+        if (nums == null || nums.length == 0 || nums.length == 1 || nums[0] == 0 ) {
+            return 0;
+        }
+        if (nums.length == 2) {
+            return 1;
+        }
+        
+        int[] dp = new int[nums.length];
+        
+        dfs(nums, dp, nums.length - 2);
+        
+        return dp[0];
+    }
+
+    private void dfs(int[] nums, int[] dp, int n) {
+        if (n < 0) {
+            return;
+        }
+        
+        int value = nums[n];
+       
+        int min = Integer.MAX_VALUE;
+        for (int j = 1; j <= value; j++) {
+            if (n + j >= dp.length) { 
+                break;
+            }
+            
+            min = Math.min(min, dp[n + j]);
+        }
+        
+        dp[n] = min == Integer.MAX_VALUE ? Integer.MAX_VALUE : min + 1;
+        System.out.println(String.format("[%s] --> %s", n, Arrays.toString(dp)));
+        
+        dfs(nums, dp, n - 1);
+    }
+
+    /**
+     * 2. BFS 
+     *    Time Limit Exceeded
+     */
+    public int jump2(int[] nums) {
+        if (nums == null || nums.length == 0 || nums.length == 1 || nums[0] == 0 ) {
+            return 0;
+        }
+        if (nums.length == 2) {
+            return 1;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(0);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            level++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int index = queue.poll();
+                if (index >= nums.length - 1) {
+                    return level - 1;
+                }
+                int value = nums[index]; 
+                for (int j = 1; j <= value; j++) {
+                    if (index + j >= nums.length - 1) {
+                        return level;
+                    }
+                    queue.offer(index + j);
+                }
+                
+            }
+        }
+        
+        return Integer.MAX_VALUE;
+    }
+
+    /**
+     * 3. 贪心，从后往前查找，每次找出跨度最大的位置
+     *    370ms - 17% //TODO ???
+     */
+    public int jump3(int[] nums) {
+//        if (nums == null || nums.length == 0 || nums.length == 1 || nums[0] == 0 ) {
+//            return 0;
+//        }
+//        if (nums.length == 2) {
+//            return 1;
+//        }
+        
+        int steps = 0;
+        
+        // 改成while, 370ms - 17% --> 279ms - 24% 
+//        for (int target = nums.length - 1; target > 0; ) {
+        int target = nums.length - 1;
+        while (target > 0) {
+            for (int i = 0; i < target; i++) {
+                if (i + nums[i] >= target) {
+                    target = i;
+                    steps++;
+                    break;
+                }
+            }
+        }
+        
+        return steps;
+    }
+
+    /**
+     * TODO 
+     * 4. 贪心 
+     *    Let's say the range of the current jump is [curBegin, curEnd], 
+     *    `reachable` is the farthest point that all points in [curBegin, curEnd] can reach. 
+     *    
+     *    Once the current point reaches `curEnd`, then trigger another jump, 
+     *    and set the new curEnd with `reachable`, then keep the above steps:
+     */
+    public int jump4(int[] nums) {
+        if (nums == null || nums.length == 0 || nums.length == 1) {
+            return 0;
+        }
+
+        int curEnd = 0;
+        int reachable = 0;
+        int steps = 0;
+        int target = nums.length - 1;
+        for (int i = 0; i < target; i++){
+            reachable = Math.max(reachable, nums[i] + i);
+            if (curEnd == i) {
+                curEnd = reachable;
+                steps++;
+            }
+//            if (reachable >= target) {
+//                return steps; 
+//            }
+        }
+        return curEnd >= nums.length - 1 ? steps : -1;
+    }
+
+    public static void main(String[] args) {
+       
+        /*
+         * Input: [2,3,1,1,4]
+         * Output: 2
+         * Explanation: The minimum number of jumps to reach the last index is 2.
+         *     Jump 1 step from index 0 to 1, then 3 steps to the last index.
+         */
+        test(new int[] {2,3,1,1,4});
+        
+        /*
+         * [2,3,0,1,4]
+         * 2
+         */
+        test(new int[] {2,3,0,1,4});
+        /*
+         * [1,2,3]
+         *  2
+         */
+        test(new int[] {1,2,3});
+        /*
+         * [5,9,3,2,1,0,2,3,3,1,0,0]
+         * 3
+         */
+        test(new int[] {5,9,3,2,1,0,2,3,3,1,0,0});
+    }
+    
+    private static void test(int[] nums) {
+        T0045_JumpGame2 sut = new T0045_JumpGame2();
+        System.out.println(Arrays.toString(nums));
+        System.out.println(sut.jump4(nums));
+    }
+
+}
